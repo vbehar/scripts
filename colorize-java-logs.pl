97f0e01f1f8bdc522ec216ad633e2fae32312961
Vincent Behar <v.behar@free.fr>
2009-03-06 14:04:42 +0100

    add CONFIG level

--- colorize-java-logs.pl
+++ colorize-java-logs.pl
@@ -7,6 +7,7 @@ s/SEVERE/\e[1;33m$&\e[0;31m/g;
 s/GRAVE/\e[1;35m$&\e[0;31m/g;
 s/ATTENTION/\e[1;31m$&\e[0;31m/g;
 s/INFO/\e[1;34m$&\e[0;34m/g;
+s/CONFIG/\e[1;34m$&\e[0;34m/g;
 s/FIN/\e[1;32m$&\e[0;32m/g;
 
 s/(\d{1,2}[ ]{1}.+\d{4}[ ]{1}\d{2}[:]{1}\d{2}[:]{1}\d{2})/\n\e[4m$&\e[0m/g;
