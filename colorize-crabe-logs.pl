be65ffc906b4a6babdc504b3ae55b41f575ce0ab
Vincent Behar <v.behar@free.fr>
2009-06-24 11:55:45 +0200

    remove new line in crabe when receiving a command

--- colorize-crabe-logs.pl
+++ colorize-crabe-logs.pl
@@ -7,8 +7,8 @@
 s/(\d{4}[-]{1}\d{2}[-]{1}\d{2}[ ]{1}\d{2}[:]{1}\d{2}[:]{1}\d{2}[.]{1}\d{3})( \[\w+ \w+\] \[\w+\] \[\w+\] )(.*)/\n\e[4m$1\e[0m$2\n$3/g; # underline
 
 # different colors for input and output
-s/^(Received from client \w+: )\n(.*)$/\e[0;32m$1\n\e[1;32m$2\e[0;32m$3/mg;   # green foreground
-s/(Replying to client \w+: )(\w+)(.*)/\e[0;34m$1\n\e[1;34m$2\e[0;34m$3/g;            # blue  foreground
+s/(Received from client \w+: )(\w+)(.*)/\e[0;32m$1\n\e[1;32m$2\e[0;32m$3/g;   # green foreground
+s/(Replying to client \w+: )(\w+)(.*)/\e[0;34m$1\n\e[1;34m$2\e[0;34m$3/g;     # blue  foreground
 
 # reset color at the end
 s/\n/\e[0m\n/g;
